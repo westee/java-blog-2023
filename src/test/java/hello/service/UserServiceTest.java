edd28124280b365b1177d65073c7c51955ea65d5
dflk <904885508@qq.com>
2023-03-15 15:34:58 +0800

    开始编写自动化测试

---
+++ src/test/java/hello/service/UserServiceTest.java
@@ -0,0 +1,62 @@
+package hello.service;
+
+import hello.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    @Mock
+    UserMapper userMapper;
+
+    @Mock
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @InjectMocks
+    UserService userService;
+
+    private final String username = "123";
+
+    @Test
+    void save() {
+        Mockito.when(bCryptPasswordEncoder.encode("123")).thenReturn("456");
+        userService.save(username, "123");
+        Mockito.verify(userMapper).insertUser(username,"456");
+    }
+
+    @Test
+    void getUserById() {
+        userService.getUserById(1);
+        Mockito.verify(userMapper).getUser(1);
+    }
+
+    @Test
+    void getUserByUsername() {
+        userService.getUserByUsername(username);
+        Mockito.verify(userMapper).getUserByUsername(username);
+    }
+
+    @Test
+    void throwExceptionWhenUserNotFound() {
+        Mockito.when(userMapper.getUserByUsername("123")).thenReturn(null);
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> userService.loadUserByUsername("123"));
+    }
+
+    @Test
+    void returnUserDetailWhenUserFound(){
+        Mockito.when(userMapper.getUserByUsername("myUser"))
+                .thenReturn(new User(123, "123", "456"));
+        UserDetails userDetails = userService.loadUserByUsername("myUser");
+
+        Assertions.assertEquals("myUser", userDetails.getUsername());
+        Assertions.assertEquals("456", userDetails.getPassword());
+    }
+}
